b3d831862cf802d2068ae870ec925a250b286de1
GamingWithKarl <101526013+GamingWithKarl@users.noreply.github.com>
2022-06-17 12:56:14 +0300

    Add files via upload

--- Kordamisküsimused - 8/password_generator.py
+++ Kordamisküsimused - 8/password_generator.py
@@ -5,12 +5,19 @@ chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!&$%^£*(
 
 #Random password loop
 while 1:
+
+    #Küsitakse kasutajalt paroolide pikkuseid ning mitu parooli tahetakse.
     password_len = int(input("What length would you like your password to be : "))
     password_count = int(input("How many passwords would you like: "))
 
+    #Loop paroolide tegemiseks
     for x in range(0, password_count):
+
+        #Parool storitakse sellesse stringi.
         password = ""
+
+        #Loopitakse nii kaua, kuna jõutakse parooli pikkusele.
         for x in range(0, password_len):
             password_char = random.choice(chars) #Võtab suvalise characteri listist.
-            password = password + password_char
-        print("Here is your password: ", password)
+            password = password + password_char #Parool pantakse stringi.
+        print("Here is your password: ", password) #Kuvatakse parool ekraanile.
--- Kordamisküsimused - 9/imageDownloader.py
+++ Kordamisküsimused - 9/imageDownloader.py
@@ -1,22 +1,35 @@
 # Pildilaadija (võtab need internetist)
-
+#Importitakse requests.
 import requests
 
 #Faili download funktsioon
 def download_files(url):
+    #Võetakse fail url'ist.
     local_filename = url.split('/')[-1]
+
+    #Võetakse fail url'ist ja laetakse alla.
     with requests.get(url, stream = True) as r:
         print("Downloading...")
+        #Kirjutatakse pildi andmed faili ning pantakse Desktopi peale.
         with open("C:/Users/karlk/Desktop/" + local_filename, 'wb') as f:
             print("Writing data to a file.")
+            #Pantakse chuck size (tähendab, kui kiiresti faili saadakse salvestada) Mida suurem, seda parem :)
             for chunk in r.iter_content(chunk_size = 8192):
+                #Kirjutatakse faili peale.
                 f.write(chunk)
+
+    #Pantakse failiedastus kinni.
     f.close()
+
+    #Kuvatakse "Download complete" ja antakse kasutajale teada genereetitud parool.
     print("Download complete")
     print("File saved as "+ local_filename)
 
+#Parooli algloop
 while 1:
+    #Prinditakse tervituskiri
     print("Welcome to the image downloader --- By Karl_Karulin")
+    #Küsitakse kasutajalt url'i ja prinditakse välja =I
     image_url = input(str("Image url: "))
     download_files(image_url)
     print("")
